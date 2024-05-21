2f7d23aff6289a5f8b819ea4b3548cd549292263
Mohammed I. Radaideh <miradaideh93@gmail.com>
2024-05-21 19:40:59 -0400

    An update considering Matt's notes

--- FEAL.py
+++ FEAL.py
@@ -59,9 +59,6 @@ def plotting (arr, arr_r, name):
     ax2.tick_params(axis='y', labelcolor='tab:blue')
     plt.savefig(name, dpi=500, bbox_inches='tight')
 
-
-
-
 # GPR training using Active Learning    
 def FE_AL (df, n_samples, spacing, pca, shuf):
     '''
@@ -70,10 +67,9 @@ def FE_AL (df, n_samples, spacing, pca, shuf):
     df :        (str) The name and directory of the data frame (.csv)
     n_samples : (int) Number of samples used in the active learning
     spacing :   (int) The increment in the number of samples make 
-                      
-    pca:        (str) choices are 'True' OR 'False', apply PCA 
-    shuf:       (str) choices are 'True' OR 'False', apply data shuffle 
-
+    pca:        (bool) choices are 'True' OR 'False', apply PCA 
+    shuf:       (bool) choices are 'True' OR 'False', apply data shuffle
+     
     Returns
     -------
     metrics: (array) an array with number of queries,Root Mean Squared Error, 
@@ -82,12 +78,12 @@ def FE_AL (df, n_samples, spacing, pca, shuf):
     '''
     # Read data csv    
     df = pd.read_csv(df)
-    if (shuf=='True'):
-        df = shuffle(df)
+    if (shuf):
+        df = shuffle(df, random_state=42)
     # define X and y
     global Xori
     # X: Use PCA
-    if (pca == 'True'):
+    if (pca):
         obj = DRAL('./cases/PCA.csv')
         Xpca,Xori = obj.PCA(var=0.95)    
     else:
@@ -108,14 +104,11 @@ def FE_AL (df, n_samples, spacing, pca, shuf):
     scaler_x = MinMaxScaler()
     scaler_x.fit(X_train)
     X_train_scale = scaler_x.transform(X_train)
-    scaler_x.fit(X_val)
     X_val_scale = scaler_x.transform(X_val)
     # y
     scaler_y = MinMaxScaler()
     scaler_y.fit(y_train)
     y_train_scale = scaler_y.transform(y_train)
-    scaler_y.fit(y_val)
-    y_val_scale = scaler_y.transform(y_val)
     # Defining Active learner
     # Defining kernel for Gaussian Process Regressor 
     kernel = DotProduct()
@@ -132,97 +125,47 @@ def FE_AL (df, n_samples, spacing, pca, shuf):
     metrics = []
     for k in range (0,len(n_queries)):
         n_query = n_queries[k]
-        global query_indx, query_inst, y_pred_inv
-        query_indx = []
-        query_inst = []
         for idx in range(n_query):
             query_idx, query_instance = regressor.query(X_train_scale)
-            query_indx.append(query_idx)
-            query_inst.append(query_instance)
             regressor.teach(X_train_scale[query_idx].reshape(1,-1), 
-                        y_train_scale[query_idx].reshape(1,-1))
+                            y_train_scale[query_idx].reshape(1,-1))
         # Trained model Prediction on unseen data
-        global y_true, y_pred_kj
         y_pred = regressor.predict(X_val_scale)
         y_pred = y_pred.reshape(-1,1)
         y_pred_kj = scaler_y.inverse_transform(y_pred)
-        # True data to calculate RMSE and R2
-        y_true = y_val
         # Metrics
-        rmse = np.sqrt(mean_squared_error(y_true, y_pred_kj))
-        r2 = r2_score(y_true, y_pred_kj)
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred_kj))
+        r2 = r2_score(y_val, y_pred_kj)
         metrics.append([n_query,rmse,r2])
         # save the model 
-        joblib.dump(regressor, "model.pkl") 
-    return np.array(metrics)
-
-
-# GPR training using random samples (Standard Training)
-def rand (df,n_samples,spacing,pca,shuf):
-    kernel = DotProduct()
-    gpr = GaussianProcessRegressor(kernel=kernel,
-                         random_state=0,
-                         n_restarts_optimizer=0,
-                         alpha=3)
-    # Read data csv    
-    df = pd.read_csv(df)
-    if (shuf=='True'):
-        df = shuffle(df)
-    # define X and y
-    if (pca == 'True'):
-        obj = DRAL('./cases/PCA.csv')
-        Xpca,Xori = obj.PCA(var=0.95)  
-    else:
-        Xori = np.array(df.drop(['letters', 'disassoc', 'assoc'], axis=1))
-           
-    y = np.array(df['assoc'])
-    # reshape y from vevtor to matrix
-    y = y.reshape(-1,1)
-    # Number of samples for validation 
-    n_val = int(len(y)) - n_samples
-    X_train, X_val, X_test = split(Xori, n_samples, n_val)
-    y_train, y_val, y_test = split(y, n_samples, n_val)
-    # Number of samples for active learning 
-    # Spacing 
-    n_queries = np.linspace(spacing,n_samples,int(n_samples/spacing),dtype=int)
-    # Scaling 
-    # X
-    scaler_x = MinMaxScaler()
-    scaler_x.fit(X_train)
-    X_train_scale = scaler_x.transform(X_train)
-    scaler_x.fit(X_val)
-    X_val_scale = scaler_x.transform(X_val)
-    # y
-    scaler_y = MinMaxScaler()
-    scaler_y.fit(y_train)
-    y_train_scale = scaler_y.transform(y_train)
-    scaler_y.fit(y_val)
-    y_val_scale = scaler_y.transform(y_val)
-    metrics = []
+        joblib.dump(regressor, "./trained_models/model"+str(n_query)+".pkl")
+        # load the model fto use it or the next iteration 
+        regressor = joblib.load("./trained_models/model"+str(n_query)+".pkl")
+    
+    #=================================================================
+    # GPR Training by Random sample
+    metrics_rand = []
     for n_samples in n_queries:
         gpr.fit(X_train_scale[0:n_samples], y_train_scale[0:n_samples])
         y_pred = gpr.predict(X_val_scale)
         y_pred = y_pred.reshape(-1,1)
         y_pred_kj = scaler_y.inverse_transform(y_pred)
-        # True data to calculate RMSE and R2
-        y_true = y_val
         # Metrics
-        rmse = np.sqrt(mean_squared_error(y_true, y_pred_kj))
-        r2 = r2_score(y_true, y_pred_kj)
-        metrics.append([n_samples,rmse,r2])
-    return np.array(metrics)     
-    
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred_kj))
+        r2 = r2_score(y_val, y_pred_kj)
+        metrics_rand.append([n_samples,rmse,r2]) 
+    return np.array(metrics), np.array(metrics_rand)
+
 
 # GPR ensemble for active learning 
 def ENAL (df, n_samples, spacing, pca, shuf, n_reg):
     # Read data csv    
     df = pd.read_csv(df)
-    if (shuf=='True'):
+    if (shuf):
         df = shuffle(df)
     # define X and y
-    global Xori
     # X: Use PCA
-    if (pca == 'True'):
+    if (pca):
         obj = DRAL('./cases/PCA.csv')
         Xpca,Xori = obj.PCA(var=0.95)    
     else:
@@ -243,14 +186,11 @@ def ENAL (df, n_samples, spacing, pca, shuf, n_reg):
     scaler_x = MinMaxScaler()
     scaler_x.fit(X_train)
     X_train_scale = scaler_x.transform(X_train)
-    scaler_x.fit(X_val)
     X_val_scale = scaler_x.transform(X_val)
     # y
     scaler_y = MinMaxScaler()
     scaler_y.fit(y_train)
     y_train_scale = scaler_y.transform(y_train)
-    scaler_y.fit(y_val)
-    y_val_scale = scaler_y.transform(y_val)
     # Defining Active learner
     # Defining kernel for Gaussian Process Regressor 
     kernel = DotProduct()
--- run.py
+++ run.py
@@ -3,118 +3,94 @@ Created on Wed May  1 18:41:57 2024
 @author: mirad
 """
 
-from FEAL import FE_AL, rand, plotting, ENAL
+from FEAL import FE_AL, plotting, ENAL
 import numpy as np
 
 # Original case
-metrics = FE_AL(df = 'data_all.csv', 
+metrics, metrics_rd = FE_AL(df = 'data_all.csv', 
                        n_samples = 30, 
                        spacing =2,
-                       pca='False',
-                       shuf='False') 
-
-metrics_rd = rand(df = 'data_all.csv', 
-                       n_samples = 30, 
-                       spacing =2,
-                       pca='False',
-                       shuf='False') 
+                       pca=False,
+                       shuf=False) 
                        
-plotting(metrics,metrics_rd,'original.png')
+plotting(metrics,metrics_rd,'./figs/original.png')
 
 #=======================================================
 # Drop A,B,C from the training data
-metrics = FE_AL(df = './cases/noABC.csv', 
+metrics, metrics_rd = FE_AL(df = './cases/noABC.csv', 
                        n_samples = 30, 
                        spacing =2,
-                       pca='False',
-                       shuf='False') 
+                       pca=False,
+                       shuf=False) 
                        
-metrics_rd = rand(df = './cases/noABC.csv', 
-                       n_samples = 30, 
-                       spacing =2,
-                       pca='False',
-                       shuf='False')
 
 plotting(metrics,metrics_rd,'./figs/noABC.png')
 
 #========================================================
 # Apply PCA to reduce the number of features 
-metrics = FE_AL(df = 'data_all.csv', 
+metrics, metrics_rd = FE_AL(df = 'data_all.csv', 
                        n_samples = 30, 
                        spacing =2,
-                       pca='True',
-                       shuf='False') 
+                       pca=True,
+                       shuf=False)
+                    
                        
-metrics_rd = rand(df = 'data_all.csv', 
-                       n_samples = 30, 
-                       spacing =2,
-                       pca='True',
-                       shuf='False')
 
 plotting(metrics,metrics_rd,'./figs/pca.png')
-#========================================================
-# Effect of adding more features from the original space  
-metrics = FE_AL(df = './cases/features.csv', 
-                       n_samples = 30, 
-                       spacing =2,
-                       pca='False',
-                       shuf='False') 
-                       
-metrics_rd = rand(df = './cases/features.csv', 
-                       n_samples = 30, 
-                       spacing =2,
-                       pca='False',
-                       shuf='False')
 
-plotting(metrics,metrics_rd,'./figs/feat.png')
 #========================================================
 # Testing model stability by running 100 case
 # The data is shuffled at the beggining of each run 
 # The run can take day(s), the results are in ./cases/shuffle_results.txt
-'''
-lst = []
-lst_rand = []
 summary = []
-n_samples = [30,40,50,60,70,80,90,100]
+n_samples = [5,10,15,20,25,30]
+#n_samples = [35,40,45,50,55,60,65,70,75,80,85,90,95,100]
 for n_samples in n_samples:
+    lst = []
+    lst_rand = []
     for i in range (1,101):
-        metrics = FE_AL(df = 'data_all.csv', 
-                        n_samples = n_samples, 
-                        spacing =3,
-                        pca='False',
-                        shuf='True') 
-
-        metrics_rd = rand(df = 'data_all.csv', 
-                          n_samples = n_samples, 
-                          spacing =3,
-                          pca='False',
-                          shuf='True') 
+        metrics, metrics_rd = FE_AL(df = 'data_all.csv', 
+                                    n_samples = n_samples, 
+                                    spacing =5,
+                                    pca=False,
+                                    shuf=True) 
         lst.append([i,metrics[-1,1],metrics[-1,2]])
         lst_rand.append([i,metrics_rd[-1,1],metrics_rd[-1,2]])
         a = np.array(lst)
         a_rand = np.array(lst_rand)
         print("Number of samples =", n_samples)
         print ("i =",i,"/100")
-        print(np.mean(a[:,1]), np.mean(a[:,2]), 
-              np.mean(a_rand[:,1]), np.mean(a_rand[:,2]))
-    summary.append([n_samples, np.mean(a[:,1]), 
-                    np.mean(a[:,2]), 
+        print(np.mean(a[:,1]),
+              np.mean(a[:,2]), 
+              np.mean(a_rand[:,1]), 
+              np.mean(a_rand[:,2]))
+    
+    summary.append([n_samples, 
+                    np.mean(a[:,1]),
+                    np.std(a[:,1]),
+                    np.mean(a[:,2]),
+                    np.std(a[:,2]),
                     np.mean(a_rand[:,1]),
-                    np.mean(a_rand[:,2])])
-'''
+                    np.std(a_rand[:,1]),
+                    np.mean(a_rand[:,2]),
+                    np.std(a_rand[:,2])])
+results = np.array(summary)
+#np.savetxt('./cases/results.txt', results)
 data = np.loadtxt('./cases/shuffle_results.txt')
 metrics = data[:,0:3]
 metrics_rand = data[:,(0,3,4)]
 plotting(metrics,metrics_rand,'./figs/shuffle.png')
+
 #===============================================================
 # Using more than one GPR 
 # RESULTS ARE UNSATISFACTORY 
 # Uncomment and Check yourself
-
+'''
 metrics = ENAL(df='data_all.csv', 
                n_samples=30, 
                spacing=2, 
-               pca='False', 
-               shuf='False', 
-               n_reg=3)
+               pca=False, 
+               shuf=False, 
+               n_reg=8)
 print(metrics)
+'''
